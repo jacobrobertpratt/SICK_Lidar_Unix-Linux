fbf193ae3af5f336cbd943fada1d7b256e302763
Jake Pratt <isuandme@gmail.com>
2019-06-13 19:18:37 -0500

    Got a connection to the TIM551 and was able to send and recieve messages. Will start producing much more here soon. WHOOOOOgit add -A ! YEAHHHHgit add -A !

--- main.c
+++ main.c
@@ -8,49 +8,57 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 int main(int argv, char * argc[]) {
     
-    int clientSocket;                                                                       /*---- ClientScoket is this personal clients socket.  ----*/
+    int clientSocket;
+    struct sockaddr_in addr;
     
-    int port_number;                                                                        /*---- Port_number is the port number that is changed for this specific client  ----*/
+    // Send buffer
+    char buffer[1024];
+    bzero(buffer, sizeof(buffer));
     
-    char buffer[1024];                                                                      /*---- Buffer is an input buffer  ----*/
+    // Recv buffer
+    char rec_buffer[1024];
+    bzero(rec_buffer, sizeof(rec_buffer));
     
-    struct sockaddr_in serverAddr;                                                          /*---- removes the '\n' since it will  ----*/
     
-    port_number = 2111;
-    
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);                                         /*---- generates the socket ----*/
-    serverAddr.sin_family = AF_INET;                                                        /*---- Specifies that we want to use TCP connection thingy ----*/
-    serverAddr.sin_port = htons(port_number);                                               /*---- sets the port number ----*/
-    serverAddr.sin_addr.s_addr = inet_addr("192.168.0.3");                                  /*---- sets the host to be local --> this is not set by the argument list above  ----*/
-    
-    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-    
-    if(connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {    /*---- Connect the socket to the server using the address struct  ----*/
+    // Create a socket
+    clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(clientSocket < 0){
         perror("ERROR");
-        exit(1);
-    } else {
-        printf("connected to the socket");
+        close(clientSocket);
+        return 0;
     }
     
-    while(1) {                                                                              /*---- loop repeats the receive / send of the client ----*/
-        
-        send(clientSocket, buffer, sizeof(buffer), 0);
-        bzero(buffer, sizeof(buffer));
-        
-        char rec_buffer[1048576];                                                           /*---- a buffer that is set empty to recieve the message from the client ----*/
-        
-        if(recv(clientSocket, rec_buffer, sizeof(rec_buffer), 0) < 0){                      /*---- recieves the socket connection message and sets it to buffer ----*/
-            perror("recv");                                                                 /*---- prints a standar output ----*/
-            break;
-        } else {
-            printf("Server: %s\n", rec_buffer);
-            bzero(rec_buffer, sizeof(rec_buffer));                                          /*---- zeros the buffer if successful ----*/
-        }
+    // Clear the sockaddr_in struct
+    memset(&addr, 0, sizeof(addr));             // Zero out structure
+    
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(2112);                // Host-2-Network byte order
+    addr.sin_addr.s_addr = inet_addr("192.168.0.2");
+    
+    if(connect(clientSocket, (struct sockaddr *)(&addr), sizeof(addr)) < 0) {
+        perror("ERROR connect: ");
+        return 0;
+    } else {
+        printf("socket connected\n");
     }
-    close(clientSocket);                                                                    /*---- closes the socket when client exits main loop ----*/
+    
+    strcpy(buffer, "\2sRN LMPscancfg\3");
+    printf("send: %s\n", buffer);
+    
+    send(clientSocket, buffer, sizeof(buffer), 0);
+    
+    if(recv(clientSocket, rec_buffer, sizeof(rec_buffer), 0) < 0){
+        perror("ERROR recv: ");
+    } else {
+        printf("recv: %s\n", rec_buffer);
+        bzero(rec_buffer, sizeof(rec_buffer));
+    }
+    
+    close(clientSocket);
     
     return 0;
 }
